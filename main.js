c043ccc90785d92416e8bb381564353f0c89f8dd
Dias <diasraphael88@gmail.com>
2021-03-09 08:51:16 +0100

    chapter1: Creating vue app

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-const name = 'James';
-
-const person = { first: name };
-
-console.log(person);
-
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
-
-sayHelloLinting('James');
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description: 'Woollen socks used to wear in winter',
+    };
+  },
+});
