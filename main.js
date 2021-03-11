0fc83f1e6a2e83ba519a581e29d0a55b9a86f1ac
Dias <diasraphael88@gmail.com>
2021-03-11 07:21:06 +0100

    added more changes

--- main.js
+++ main.js
@@ -3,6 +3,35 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       description: 'Woollen socks used to wear in winter',
+      url: 'http://vuemastery.com',
+      greenSocks: './assets/images/socks_green.jpg',
+      blueSocks: './assets/images/socks_blue.jpg',
+      onSale: false,
+      inventory: 3,
+      sizes: ['small', 'medium', 'large'],
+      colors: [
+        { color: 'green', id: '501' },
+        { color: 'blue', id: '502' },
+      ],
+      cart: 0,
+      variant: true,
+      brand: 'Jack and Jones',
     };
+  },
+  methods: {
+    increment() {
+      this.cart += 1;
+    },
+    decrement() {
+      this.cart -= 1;
+    },
+    changeColor(color) {
+      this.variant = color.color === 'green' ? true : false;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
   },
 });
